b1fae20734036dd9814a2a6f8101919bb76c4a83
James Waweru <57865213+KEdev-jimmy@users.noreply.github.com>
2026-09-20 20:59:57 +0300

    Fix opening screen graphicsLayer import

--- app/src/main/java/ke/co/nsewatcher/SplashIntro.kt
+++ app/src/main/java/ke/co/nsewatcher/SplashIntro.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
