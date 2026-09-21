219a7ec4fadcf36b7be7e26e51933e48838f9351
James Waweru <57865213+KEdev-jimmy@users.noreply.github.com>
2026-09-21 11:05:06 +0300

    Remove unused skyline resource import

--- app/src/main/java/ke/co/nsewatcher/MarketDashboard.kt
+++ app/src/main/java/ke/co/nsewatcher/MarketDashboard.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.res.painterResource
 import coil3.compose.AsyncImage
 import ke.co.nsewatcher.data.MyStocksCache
 import kotlinx.coroutines.async
