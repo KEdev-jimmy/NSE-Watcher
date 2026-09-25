c0b0aa5146aed10d244d1f7d7425bc310b09997b
James Waweru <57865213+KEdev-jimmy@users.noreply.github.com>
2026-09-25 19:14:54 +0300

    Fix premium Companies imports

--- app/src/main/java/ke/co/nsewatcher/PremiumCompaniesDirectory.kt
+++ app/src/main/java/ke/co/nsewatcher/PremiumCompaniesDirectory.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
